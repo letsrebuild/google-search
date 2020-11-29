e048fd245b941793160d4cd9650683f925c2471f
letsrebuild <letsrebuilditnow@gmail.com>
2020-11-29 11:36:33 +0530

    Added favicon, logo and components for header, footer and search

--- apps/google-search/src/app/app.component.ts
+++ apps/google-search/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@lets-rebuild/api-interfaces';
 
 @Component({
   selector: 'lets-rebuild-root',
@@ -8,6 +6,5 @@ import { Message } from '@lets-rebuild/api-interfaces';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+  
 }
--- apps/google-search/src/app/app.module.ts
+++ apps/google-search/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, SearchComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
