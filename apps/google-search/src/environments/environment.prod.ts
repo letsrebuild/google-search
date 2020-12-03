7f5fd8c58e739dd0b423212121baa5cc7c866c2c
letsrebuild <letsrebuilditnow@gmail.com>
2020-12-03 08:33:59 +0530

    Updated env variables for production

--- apps/google-search/src/environments/environment.prod.ts
+++ apps/google-search/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: true,
+  backend: 'https://nx-google-search-api.herokuapp.com/api/dictionary/search/',
+  googleSearchApi: 'https://www.google.com/search?q=',
+  googleDoodle: 'https://www.google.com/doodles/'
 };
